67f3c46cc59f59d5ea0276e6cd7b5897767dd3cb
Manfred Lagos Rodriguez <mlagosr@ucenfotec.ac.cr>
2025-07-13 16:52:35 -0600

    [New] Se establece la estuctuta del front-end con layout la reutilizar código

--- back-end/index.js
+++ back-end/index.js
@@ -3,36 +3,41 @@ const express = require('express'); //Facilita la creaciÃ³n de servidores y ma
 const mongoose = require('mongoose'); //Permite conectarse a la BD de mongoDB y crear las colecciones y realizar consultas
 const cors = require('cors'); //Permite la comunicaciÃ³n entre dominios diferentes
 const bodyParser = require('body-parser'); //Permite interpretar los datos que vienen en la peticiÃ³n en formto json
+const layouts = require("express-ejs-layouts");
 
 require('dotenv').config(); //Se importa el archivo .env para poder utilizar sus variables dentro del cÃ³digo
 
+
 const app = express(); //Crear una instancia de express
 const PORT = process.env.PORT || 3000; //Usar el puerto indicado en .env o si no se indica usar el puerto 3000
+const path = require('path');
 
 //importar rutas
-const usuarioRoute = require("./routes/usuario.route")
-const productoRoute = require("./routes/producto.route")
 const usuario_mepRoute = require("./routes/usuario_mep.route")
 
 app.use(express.json());//Habilita el manejo de JSON en las peticiones
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());//Habilita el anÃ¡lisis de JSON en las peticiones 
 app.use(cors());
+app.use(layouts);
+app.use(express.static(path.join(__dirname, "public")))
+app.set('layout', 'layouts/layout');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'front-end/views'));
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(()=> console.log('MongoDB Atlas conectado'))
-.catch(error => console.log('OcurriÃ³ un error al conectarse con MongoDB: ', error));
+.catch(error => console.log('Ocurrió un error al conectarse con MongoDB: ', error));
 
 //rutas
-app.use("/usuarios", usuarioRoute)
-app.use("/productos", productoRoute)
 app.use("/usuario_mep", usuario_mepRoute)
 
-app.get('/', (req,res)=> {
-    res.send('Servidor en funcionamiento');
+app.get('/', (req, res) => {
+    res.render("index.ejs");
 });
 
 app.listen(PORT, ()=>{
