f70d513d382ba8134753d5f470d8dd28b421930a
Manfred Lagos Rodriguez <mlagosr@ucenfotec.ac.cr>
2025-07-22 18:38:35 -0600

    [Problem] Se corrigen errores  de navegación entre páginas

--- back-end/src/routes/contacto.router.js
+++ back-end/src/routes/contacto.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/contacto.controller");
 
-router.get("/Contacto", controller.index);
+router.get("/", controller.index);
 
 module.exports = router;
--- back-end/src/routes/sobreNosotros.router.js
+++ back-end/src/routes/sobreNosotros.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/sobreNosotros.controller");
 
-router.get("/SobreNosotros", controller.index);
+router.get("/", controller.index);
 
 module.exports = router;
